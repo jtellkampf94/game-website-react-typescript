89edce2737b23d9a4762b2004e72d07eda07378c
Jonathan <jtellkampf@gmail.com>
2021-04-05 12:25:11 +0100

    Created globalstyles component and included it in the app

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,14 +1,18 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, Fragment } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import GlobalStyles from 'components/GlobalStyles'
 
 import Home from 'components/Home'
 
 const App = (): ReactElement => (
-	<BrowserRouter>
-		<Switch>
-			<Route path="/" component={Home} />
-		</Switch>
-	</BrowserRouter>
+	<Fragment>
+		<GlobalStyles />
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" component={Home} />
+			</Switch>
+		</BrowserRouter>
+	</Fragment>
 )
 
 export default App
