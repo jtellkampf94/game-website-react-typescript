da04e957add8d5274c481f708277d82aac08c509
Jonathan <jtellkampf@gmail.com>
2021-04-05 15:06:56 +0100

    Styled game filter component

--- src/components/GameFilter/index.tsx
+++ src/components/GameFilter/index.tsx
@@ -1,57 +1,58 @@
 import React, { ChangeEvent, ReactElement } from 'react'
 
 import { PLATFORMS, GENRES, TAGS, SORT_BY } from './constants'
+import { Form, Label, Select } from './styles'
 
 interface Props {
 	onChange: (e: ChangeEvent<HTMLFormElement>) => void
 }
 
 const GameFilter = ({ onChange }: Props): ReactElement => (
-	<form onChange={onChange}>
-		<label htmlFor="platform-select">
+	<Form onChange={onChange}>
+		<Label htmlFor="platform-select">
 			Platform:
-			<select name="platform" id="platform-select">
+			<Select name="platform" id="platform-select">
 				{PLATFORMS.map(platform => (
 					<option key={platform.value} value={platform.value}>
 						{platform.display}
 					</option>
 				))}
-			</select>
-		</label>
+			</Select>
+		</Label>
 
-		<label htmlFor="genre-select">
+		<Label htmlFor="genre-select">
 			Genre:
-			<select name="genre" id="genre-select">
+			<Select name="genre" id="genre-select">
 				{GENRES.map(genre => (
 					<option key={genre.value} value={genre.value}>
 						{genre.display}
 					</option>
 				))}
-			</select>
-		</label>
+			</Select>
+		</Label>
 
-		<label htmlFor="tag-select">
+		<Label htmlFor="tag-select">
 			Tag:
-			<select name="tag" id="tag-select">
+			<Select name="tag" id="tag-select">
 				{TAGS.map(tag => (
 					<option key={tag.value} value={tag.value}>
 						{tag.display}
 					</option>
 				))}
-			</select>
-		</label>
+			</Select>
+		</Label>
 
-		<label htmlFor="sortBy-select">
+		<Label htmlFor="sortBy-select">
 			Sort By:
-			<select name="sortBy" id="sortBy-select">
+			<Select name="sortBy" id="sortBy-select">
 				{SORT_BY.map(sortBy => (
 					<option key={sortBy.value} value={sortBy.value}>
 						{sortBy.display}
 					</option>
 				))}
-			</select>
-		</label>
-	</form>
+			</Select>
+		</Label>
+	</Form>
 )
 
 export default GameFilter
