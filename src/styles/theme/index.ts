0c093c1385b33aeb0dc0ac5aeace37c58d3274ef
Jonathan <jtellkampf@gmail.com>
2021-04-05 12:16:09 +0100

    Defined global styles and themes

---
+++ src/styles/theme/index.ts
@@ -0,0 +1,9 @@
+export const primaryColor = '#272b30'
+export const secondaryColor = '#32383e'
+
+export const primaryTextColor = '#aaa'
+export const secondaryTextColor = '#7a8288'
+export const teritaryTextColor = '#272b30'
+
+export const backgroundColor = '#7a8288'
+ 
